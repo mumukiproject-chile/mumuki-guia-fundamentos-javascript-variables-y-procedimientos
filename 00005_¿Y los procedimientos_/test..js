78103a0cb76af95aedb3aa002d301d476f57fa56
agus@mumuki.org <agus@mumuki.org>
2020-07-15 19:59:19 +0000

    Mumuki Export on 2020-07-15 19:59:19 +0000

--- 00005_¿Y los procedimientos_/test..js
+++ 00005_¿Y los procedimientos_/test..js
@@ -1,9 +1,9 @@
 describe('versos', function () {
     it('deberia imprimir los versos', function () {
-        versosMartinFierro();
+        versosTierraChilena();
         assert.equal(fakeConsole.toString(), 'Danzamos en tierra chilena,\nmás bella que Lía y Raquel;\nla tierra que amasa a los hombres\nde labios y pecho sin hiel...\n');
     });
     it('no deberia retornar nada', function () {
-        assert(versosMartinFierro() === undefined);
+        assert(versosTierraChilena() === undefined);
     });
 });
