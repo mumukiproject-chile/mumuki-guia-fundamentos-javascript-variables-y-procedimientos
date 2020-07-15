976b394c805f82d9be8c330b5d58b9dc075de1a2
agus@mumuki.org <agus@mumuki.org>
2020-07-15 19:50:58 +0000

    Mumuki Export on 2020-07-15 19:50:58 +0000

--- 00010_Tierra Chilena/test..js
+++ 00010_Tierra Chilena/test..js
@@ -1,7 +1,7 @@
 describe('versos', function () {
     it('deberia imprimir los versos', function () {
         versosTierraChilena();
-        assert.equal(fakeConsole.toString(), 'Danzamos en tierra chilena\nmás bella que Lía y Raquel;\nla tierra que amasa a los hombres\nde labios y pecho sin hiel...\n');
+        assert.equal(fakeConsole.toString(), 'Danzamos en tierra chilena,\nmás bella que Lía y Raquel;\nla tierra que amasa a los hombres\nde labios y pecho sin hiel...\n');
     });
     it('deberia retornar 0', function () {
         assert.equal(versosTierraChilena(), 0);
